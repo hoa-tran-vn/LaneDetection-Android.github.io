857e02449999c9bdd299dd5540221283c42f2181
hoa-tran-vn <64316354+hoa-tran-vn@users.noreply.github.com>
2020-06-18 16:26:52 +0700

    Update com_example_opencvprject_OpenCvNativeClass.cpp

--- com_example_opencvprject_OpenCvNativeClass.cpp
+++ com_example_opencvprject_OpenCvNativeClass.cpp
@@ -20,11 +20,11 @@ void detect(Mat& src){
    //Lấy ngưỡng để loại bỏ các vùng không mong muốn
     threshold(dst, cdst, 250, 200, THRESH_BINARY_INV );
     // nhận diện đường biên
-    Canny(cdst, dst, 190, 230, 5, true);
+    Canny(cdst, dst, 190, 230, 7, true);
 
     // tạo trường vector để lưu các điểm
     vector<Vec4i> linesP;
-    HoughLinesP(dst, linesP, 1, CV_PI/180, 43, 35, 100 );
+    HoughLinesP(dst, linesP, 1, CV_PI/180, 43, 35, 60 );
     // vẽ làn đường
     for( size_t i = 0; i < linesP.size(); i++ )
     {
